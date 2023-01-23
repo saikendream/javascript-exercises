c0ccac0daaf0659633dcb0ffe002d51ce13c844b
jae <121826115+saikendream@users.noreply.github.com>
2023-01-23 15:57:09 -0300

    Done exercises 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let result = "";
+
+    for(let i = 0; i < num; i++) {
+        result = result + string;
+    }
+
+    if(num < 0) {
+        result = "ERROR";
+    }
+
+    return result;
 
 };
 
