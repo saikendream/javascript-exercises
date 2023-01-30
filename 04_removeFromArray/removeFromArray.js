d5b8eb9cfa3035f46c96c82b8800673c94c2d3fc
jae <121826115+saikendream@users.noreply.github.com>
2023-01-30 14:29:50 -0300

    Write and fix removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...del) {
+    // Creates an empty array to store the expected return
+    let result = [];
 
+    // Goes through every single element given to "arr"
+    arr.forEach((element) => {
+        // If "del" DOES NOT UNCLUDE said element;
+        if(!del.includes(element)) {
+            // Push said element to "result"
+            result.push(element);
+        }
+    })
+
+    return result;
 };
 
 // Do not edit below this line
