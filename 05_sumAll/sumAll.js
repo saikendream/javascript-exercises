98b91ba59cbd9d8e02616549b1730489b758e177
jae <121826115+saikendream@users.noreply.github.com>
2023-01-30 15:31:26 -0300

    Write sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    // Creates an empty variable where
+    // the sum shall be stored
+    let result = 0;
 
+    if(a < 0 || b < 0 ||  typeof a !== 'number' || typeof b !== 'number'){
+        result = "ERROR";
+    } else if(a < b){
+        while(a < b) {
+            let c = a;
+            result = result + c;
+            a = a + 1;
+        }
+
+        result = result + b;
+    } else if(a > b){
+        while(b < a) {
+            let c = b;
+            result = result + b;
+            b = b + 1;
+        }
+
+        result = result + a;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
