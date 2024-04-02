4a009dcaff596f70b9edb7157b3b9d0073b7a880
Jae <121826115+saikendream@users.noreply.github.com>
2024-04-02 11:38:09 -0300

    Solved PALINDROMES assignement

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    input = input.replace(/[^a-zA-Z]/g, "");
+    input = input.toUpperCase();
 
+    let charCount = input.length;
+    let pali = 0;
+
+    for (i = 0; i < charCount; i++) {
+        if(input[i] == (input[(charCount - (i + 1))])) {
+            pali = pali + 1;
+        };
+    };
+
+    if(pali == (charCount)) {
+        return true;
+    } else {return false;};
 };
 
 // Do not edit below this line
