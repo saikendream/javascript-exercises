24ced1ee686d56c2e51e5b88173afbfe62522960
Jae <121826115+saikendream@users.noreply.github.com>
2024-04-02 11:37:03 -0300

    Solved FIBONACCI assignement

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    let fibo = [0, 1];
+    index = parseInt(index);
+    index = index - 1;
+    let n = (fibo.length) - 1;
+
+    for(i = 0; i < index; i++) {
+        fibo.push(fibo[(n - 1)] + fibo[n]);
+        n = (fibo.length) - 1;
+    }
+
+    if(index < 0) {
+        return "OOPS";
+    } else { return fibo[(index + 1)]; };
 
 };
 
